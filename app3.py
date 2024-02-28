d32faaa2b1b10e54dabb01a2b6d870312beed4bf
vimal20021225 <146183002+vimal20021225@users.noreply.github.com>
2024-02-28 09:09:12 +0530

    Add files via upload

---
+++ app3.py
@@ -0,0 +1,30 @@
+from googletrans import Translator
+import streamlit as st
+def translate_text(text, target_language):
+    translator = Translator()
+    translation = translator.translate(text, dest=target_language)
+    return translation.text
+import pyttsx3
+import speech_recognition 
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[0].id)
+rate = engine.setProperty("rate",170)
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+def main():
+    st.title("Multi-Language Translator")
+    text = st.text_input("Enter the text to translate")
+    source_lang = st.text_input("Enter the source language (leave empty for auto-detection)")
+    target_lang = st.text_input("Enter the target language: ")
+    if st.button("Translate"):
+        if not source_lang:
+            source_lang = 'auto'
+
+        translated_text = translate_text(text, target_lang)
+        speak(translated_text)
+        st.success({translated_text})
+
+if __name__ == "__main__":
+    main()
