c2cb329b3a2490091c2f79a2671f0c3d9f924c9c
vimal20021225 <146183002+vimal20021225@users.noreply.github.com>
2024-03-01 08:01:57 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -1,21 +1,31 @@
-from googletrans import Translator
 import streamlit as st
-def translate_text(text, target_language):
-    translator = Translator()
-    translation = translator.translate(text, dest=target_language)
-    return translation.text
-def main():
-    st.title("Multi-Language Translator")
-    text = st.text_input("Enter the text to translate")
-    source_lang = st.text_input("Enter the source language (leave empty for auto-detection)")
-    target_lang = st.text_input("Enter the target language: ")
-    if st.button("Translate"):
-        if not source_lang:
-            source_lang = 'auto'
+from textblob import TextBlob
+from transformers import pipeline
+from streamlit_extras.let_it_rain import rain
+st.title("Sentiment Analysis App")
+pipe = pipeline(model = "lxyuan/distilbert-base-multilingual-cased-sentiments-student")
 
-        translated_text = translate_text(text, target_lang)
-        speak(translated_text)
-        st.success({translated_text})
 
-if __name__ == "__main__":
-    main()
+message=st.text_input("Please Enter your text")
+if st.button("Analyze the Sentiment"):
+    output_model = pipe(message)
+    result = output_model[0]['label']
+    if result == "positive":
+        st.warning("The predicted sentiment is positive!!")
+        rain(emoji="😄",
+             font_size=30,
+             falling_speed=2,
+             animation_length=30,)
+    elif result=="neutral":
+        st.warning("The predicted sentiment is neutral")
+        rain(emoji="😑",
+             font_size=30,
+             falling_speed=2,
+             animation_length=30,)
+    else:
+        st.warning("The predicted sentiment is negative")
+        rain(emoji="😠",
+             font_size=30,
+             falling_speed=2,
+             animation_length=30,)
+    st.success(result)
